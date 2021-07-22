5435e5247ec9fdd4414899c69477a3d66ece92f3
deyso <bauer.dezso@gmail.com>
2021-07-22 13:32:33 +0200

    test live

--- docs/pw.js
+++ docs/pw.js
@@ -1,11 +1,12 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1626946205.834249
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1626953396.895832
 importScripts('https://aswpsdkeu.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://www.sportico.com/wp\u002Dcontent/uploads/2021/06/spArt.jpg?w\u003D681\u0026h\u003D383\u0026crop\u003D1',
   defaultTitle: 'Test title',
   defaultActionURL: 'https://deyso.github.io/airshiptest/',
-  appKey: '5RMQoVNmTZKkRQzSWsP88A',
-  token: 'MTo1Uk1Rb1ZObVRaS2tSUXpTV3NQODhBOk81MjdOTDFJd0NFcUZpMHFPNzFKdmRmSmVzZGhJSGF1MGhjOVNqWXViTnc',
-  vapidPublicKey: 'BEUeipTLa20gngcbHSpaZOu0E3oI7gyC3NAYoKz5tDolUg_cgZRnk-W8jaWJbZRAAiablr0KCRR892MxGyRiHO0='
+  appKey: 'rRJ1jEJcRPeQ72sS2sH4oQ',
+  token: 'MTpyUkoxakVKY1JQZVE3MnNTMnNING9ROk9TNmZ2LUtnY3NNZzJabl85NTlHUlBCdkx4N09hVjlDMlRBeTJzRWZBOEU',
+  vapidPublicKey: 'BFvMeGVgUVDH8JN9NhVwS5oOGUc4AvaEkWCVpCthnl6VkpueOfTovlqFfFsVxL_E66BxyOTrWtMWySFVWGysA28='
 })
+
 console.log(self, 'self with webpack')
