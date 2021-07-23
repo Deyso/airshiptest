f4f8a719e3a41588eb3563ac78a7a0587ac2744c
deyso <bauer.dezso@gmail.com>
2021-07-23 06:59:32 +0200

    testing message

--- src/push-worker.js
+++ src/push-worker.js
@@ -1,10 +1,10 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1627015179.9896472
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1627016307.8274815
 importScripts('https://aswpsdkeu.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://www.sportico.com/wp\u002Dcontent/uploads/2021/06/spArt.jpg?w\u003D681\u0026h\u003D383\u0026crop\u003D1',
   defaultTitle: 'Test title',
-  defaultActionURL: 'https://local.performgroup.com/',
-  appKey: 'rRJ1jEJcRPeQ72sS2sH4oQ',
-  token: 'MTpyUkoxakVKY1JQZVE3MnNTMnNING9ROk9TNmZ2LUtnY3NNZzJabl85NTlHUlBCdkx4N09hVjlDMlRBeTJzRWZBOEU',
-  vapidPublicKey: 'BFvMeGVgUVDH8JN9NhVwS5oOGUc4AvaEkWCVpCthnl6VkpueOfTovlqFfFsVxL_E66BxyOTrWtMWySFVWGysA28='
+  defaultActionURL: 'https://deyso.github.io/airshiptest/',
+  appKey: '5RMQoVNmTZKkRQzSWsP88A',
+  token: 'MTo1Uk1Rb1ZObVRaS2tSUXpTV3NQODhBOk81MjdOTDFJd0NFcUZpMHFPNzFKdmRmSmVzZGhJSGF1MGhjOVNqWXViTnc',
+  vapidPublicKey: 'BEUeipTLa20gngcbHSpaZOu0E3oI7gyC3NAYoKz5tDolUg_cgZRnk-W8jaWJbZRAAiablr0KCRR892MxGyRiHO0='
 })
